6218c270aed964ba3e535d20f0f1cad83c8908a5
MiamboHamiro-sys <hbmhal@gmail.com>
2026-09-17 11:56:19 +0200

    Update RoboticaMatematica.py

--- RoboticaMatematica.py
+++ RoboticaMatematica.py
@@ -218,7 +218,7 @@ elif st.session_state.ecra == 2:
             with st.chat_message("assistant"):
                 try:
                     res = client.chat.completions.create(
-                        model="llama3-70b-8192",
+                        model="llama-3.1-8b-instant",
                         messages=[{"role": "system", "content": SYSTEM_PROMPT}] + st.session_state.mensagens
                     )
                     texto = res.choices[0].message.content
